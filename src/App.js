301a275c25a110a5b84ee2eaf3b461454eb9a03b
Bart Dority <b0rgBart3@gmail.com>
2021-01-14 14:31:10 -0800

    adding more presets

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import {
 
 
 function Presets() {
-  let presets = ["Apollo 11", "mars", "Space Shuttle", "Hubble", "Moon"];
+  let presets = ["Apollo 11", "mars", "Space Shuttle", "Hubble", "Moon", "weightless", "gravity","zero gravity", "g-force", "lunar landing","Dark Side", "Black Holes", "pleiades", "pleiades star cluster", "Galaxy", "galaxies", "solar system", "star death","Milky Way", "Orion", "Lockheed Martin", "Jet propulsion", "Saturn", "Pluto","Venus", "Jupiter", "Jupiter's Moons", "LEWIS RESEARCH CENTER", "Space Station", "Earth", "Prometheus", "Pandora", "Cassini spacecraft", "Voyager", "Carl Sagan", "Chandra X-ray Observatory", "supernova", "Galaxy Evolution Explorer", "Spitzer Space Telescope", "radio emissions", "x-rays", "dark matter", "space walk", "Endeavour", "Bill Nye", "Neil deGrasse Tyson", "Cosmos", "Cape Canaveral", "Launch Pad", "Kennedy Space Center"];
   const dispatch = useDispatch();
 
   function setPreset(preset) {
@@ -28,9 +28,9 @@ function Presets() {
   return (
     <div>
       <h1>Presets</h1>
-      <ul>
+      <ul className='presetList'>
         {presets.map((preset, index) => (
-          <li key={index} onClick={()=>setPreset(preset)}>{preset}</li>
+          <li key={index} onClick={()=>setPreset(preset)} className="presetLink">{preset}</li>
         ))}
       </ul>
     </div>
