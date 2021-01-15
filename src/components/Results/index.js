9126efaa85682b426077f1228b899a9c5f4bdb05
Bart Dority <b0rgBart3@gmail.com>
2021-01-14 18:36:28 -0800

    adding material icons in a centered flex container

--- src/components/Results/index.js
+++ src/components/Results/index.js
@@ -1,6 +1,9 @@
 
 import React, { useState, useEffect, useRef } from "react";
 import { useSelector, useDispatch } from 'react-redux';
+import playIcon from "../../assets/play_circle_outline-24px.svg";
+import prevIcon from "../../assets/skip_previous-24px.svg";
+import nextIcon from "../../assets/skip_next-24px.svg";
 import {
   selectResult,
   newResults,
@@ -34,7 +37,21 @@ function Results() {
              <div className="description">{result[slideNumber].data[0].description}</div> :
              <p></p>
          }
-         <div onClick={prev}>Previous</div><div onClick={next}>Next</div>
+         <div className="slideNavigation flex-container center">
+            <div className='navWrapper flex-container center'>
+                <div onClick={prev} >
+                <img src={prevIcon} className="icon"></img>
+                </div>
+                <div className="playButton">
+                    <img src={playIcon} className="icon"></img>
+                </div>
+                <div onClick={next} >
+                <img src={nextIcon} className="icon"></img>
+                </div>
+            </div>
+         </div>
+         
+    
             {/* {result.map((item, index)=>(
                 <p key={index}>{item.data[0].description}
                 { item.links && item.links[0] ? (
